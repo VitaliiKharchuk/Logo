ab6e60f62c56feb8990494630b29486be0c3623a
GupCa <anntytova@gmail.com>
2017-10-28 19:18:41 +0300

    Start working with authZ on client side.

--- Logo/Logo.Web/Logo.Web.UI/src/app/app.module.ts
+++ Logo/Logo.Web/Logo.Web.UI/src/app/app.module.ts
@@ -1,22 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser'; 
-import { NgModule } from '@angular/core'; 
- 
-import { AppComponent } from './app.component'; 
-import { FormsModule } from '@angular/forms'; 
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MenuComponent } from './components/menu/menu.component'; 
 
-@NgModule({ 
-    declarations: [ 
-        AppComponent, MenuComponent 
-    ], 
-    imports: [ 
-        BrowserModule, 
-        FormsModule, 
-        HttpModule 
-    ], 
-    providers: [], 
-    bootstrap: [AppComponent] 
-})
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers/index';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
+
+import { routing } from './app.routing';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './_components/menu/menu.component';
+import { AuthentificationGuard } from './_guards/index';
+import { AlertComponent } from './_directives/alert/alert.component';
+import { HomeComponent } from './_components/home/home.component';
+import { LoginComponent } from './_components/login/login.component';
+import { RegisterComponent } from './_components/register/register.component';
+import { AlertService, AuthentificationService, UserService } from './_services/index';
+
+@NgModule({
+    declarations: [
+        AppComponent, MenuComponent, AlertComponent, HomeComponent, LoginComponent, RegisterComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        routing
+    ],
+    providers: [
+        AuthentificationGuard,
+        AlertService,
+        AuthentificationService,
+        UserService,
  
-export class AppModule {}
+        // providers used to create fake backend
+        fakeBackendProvider,
+        MockBackend,
+        BaseRequestOptions
+    ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
